db657ebdb2a50d9555fb701412f5c6f9693789a0
Grant Porter <gtporter@gmail.com>
2018-06-10 19:19:00 -0500

    First failing test

---
+++ README.md
@@ -0,0 +1,2 @@
+# Grant Porter - Pillar Vending Machine Kata - 6/10/2018
+
---
+++ VendingMachineKata.Tests/DisplayTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VendingMachineKata.Tests
+{
+    [TestClass]
+    public class DisplayTests
+    {
+        [TestMethod]
+        public void WhenTheMachineIsReadyToAcceptMoney_TheDisplayReadsInsertCoin()
+        {
+            Equals(VendingMachine.GetDisplay(), @"INSERT COIN");
+        }
+    }
+}
