78d6ff2566abf47071dceb1d5f3d8bf968ce23bc
Grant Porter <gtporter@gmail.com>
2018-06-12 22:53:32 -0500

    Code Cleanup - Remove unused references

--- VendingMachineKata.Library/DispenserChannel.cs
+++ VendingMachineKata.Library/DispenserChannel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VendingMachineKata.Library
+﻿namespace VendingMachineKata.Library
 {
     public class DispenserChannel
     {
